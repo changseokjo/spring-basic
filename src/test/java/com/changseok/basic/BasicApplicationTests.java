97637d6293b796518c02a4b17c1148ca4266b1f6
changseokjo <yulini1022@gmail.com>
2023-09-14 18:10:15 +0900

    get, post, put, patch, delete Mapping 개념정리

---
+++ src/test/java/com/changseok/basic/BasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.changseok.basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
