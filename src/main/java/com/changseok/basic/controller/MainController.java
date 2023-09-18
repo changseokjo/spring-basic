021c4eef11b227f79790a344adcb084045297fde
changseokjo <yulini1022@gmail.com>
2023-09-18 16:12:50 +0900

    study-20230918

--- src/main/java/com/changseok/basic/controller/MainController.java
+++ src/main/java/com/changseok/basic/controller/MainController.java
@@ -2,7 +2,6 @@ package com.changseok.basic.controller;
 
 import javax.validation.Valid;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -19,29 +18,41 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.changseok.basic.dto.request.PatchValidationDto;
 import com.changseok.basic.dto.request.PostRequestBodyDto;
+import com.changseok.basic.dto.request.PostUserRequestDto;
+import com.changseok.basic.dto.response.PostUserResponseDto;
 import com.changseok.basic.dto.response.TmpResponseDto;
 import com.changseok.basic.service.MainService;
-import com.changseok.basic.service.implement.MainServiceImplement;
 
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
 // description: Controller - 레이어드 아키텍처 상의 프레젠테이션 영역 //
 // description: 클라이언트로부터 요청(입력)을 받고 서비스 결과를 응답(출력)하는 영역 //
 
-// description: @RestController - REST API 형식 Controller를 만들고자 할때 사용하는 어노테이션 //
+// description: @RestController - REST API 형식 Controller를 만들고자 할 때 사용하는 어노테이션 //
 // description: Response Body의 타입이 JSON 형태의 데이터를 반환 //
 @RestController
-// description: @ResquestMapping - Request의 URL 패턴에 따라 클래스 및 메소드를 결정하는 어노테이션 //
-@RequestMapping("/")  // http://localhost:4000/**
-// @RequestMapping("/main") http://localhost:4000/main/**
+// description: @RequestMapping - Request의 URL 패턴에 따라 클래스 및 메소드를 결정하는 어노테이션 //
+@RequestMapping("/")    // http://localhost:4000/**
+// @RequestMapping("/main")   ex) http://localhost:4000/main/**
 @RequiredArgsConstructor
 public class MainController {
 
+    // description: @Autowired - Java bean으로 등록되어 있는 클래스에 대해서 제어의 역전을 통해 의존성을 주입하는 어노테이션 //
+    // @Autowired
+    // description: IoC를 통해서 DI 하는 방법 //
+    // description: 1. 멤버변수를 사용한 DI //
+    // description: 2. setter 메서드를 사용한 DI //
+    // description: 3. 생성자를 사용한 DI //
+
+    // description: Spring Framework 공식 문서상에서 생성자를 사용한 DI를 권장 //
+    // description: 생성자를 사용한 DI에서는 @Autowired를 사용하지 않아도 됨 //
+
+    // description: 아래 방법은 생성자를 사용한 IoC를 통한 DI이며 final로 지정하여 필수 멤버 변수로 지정 함 //
+    // description: lombok 라이브러리의 @RequiredArgsConstructor를 사용하여 필수 멤버 변수의 생성자를 만듦 //
     private final MainService mainService;
 
     // http://localhost:4000/hello GET
-    @RequestMapping(value="hello", method={RequestMethod.POST})
+    @RequestMapping(value="hello", method = {RequestMethod.POST})
     public String hello() {
         return "Hello Spring framework!!";
     }
@@ -59,7 +70,7 @@ public class MainController {
     // description: 데이터 입력시 Body로 입력 //
     @PostMapping("")
     public String postMethod() {
-        return "This method is Post Method";
+        return "This method is Post method";
     }
 
     // description: @RequestMapping 중 PUT Method에 한정하여 인식 //
@@ -67,7 +78,7 @@ public class MainController {
     // description: 데이터 입력시 Body로 입력 //
     @PutMapping("")
     public String putMethod() {
-        return "This mehtod is Put method";
+        return "This method is Put method";
     }
 
     // description: @RequestMapping 중 PATCH Method에 한정하여 인식 //
@@ -86,7 +97,7 @@ public class MainController {
         return "This method is Delete method";
     }
 
-    // description: @PathVariabale - Path 자체를 변수의 값으로 인식 //
+    // description: @PathVariable - Path 자체를 변수의 값으로 인식 //
     // description: {변수명}로 표현 -> @PathVariable("변수명")로 받음 //
     @GetMapping("path-variable/{variable}")
     public String getPathVariable(
@@ -96,29 +107,29 @@ public class MainController {
     }
 
     // description: @RequestParam - Query Parameter로 Key와 Value를 받아옴 //
-    // description: Query Parameter - ?name1=value1&name2=value2... //
+    // description: Query Parameter - ?name1=value1&name2=vaule... //
     // description: @RequestParam("name1") -> name1에 대한 value1를 받음 //
     @GetMapping("parameter")
     public String getParameter(
         @RequestParam("name") String name,
         @RequestParam("age") Integer age
     ) {
-        return "name: " + name + ", age: " + age;
+        return "name " + name + ", age: " + age;
     }
 
-    // description: @RequestBody - Request의 Body에 포함된 데이터를 받아옴 //
+    // description: @RequestBody - Request에 Body에 포함된 데이터를 받아옴 //
     // description: 문자열 혹은 객체로 받을 수 있음 //
     @PostMapping("request-body")
     public String postRequestBody(
         // @RequestBody String requestBody
         @RequestBody PostRequestBodyDto requestBody
-    ) {
-        return "Request의 Body는 " + requestBody.getName() + " " + requestBody.getAge() + " 입니다.";
+    ){
+        return "Request의 Body는 " + requestBody.getName() + " " + requestBody.getAge() + "입니다.";
     }
 
     @PatchMapping("validation")
     public String validation(
-        // description: DTO에 작성된 유효성 검사를 적용하려한다면 @Valid 를 매개변수 자리에 추가해줘야함 //
+        // description: DTO에 작성된 유효성 검사를 적용하려 한다면 @Valid 를 매개변수 자리에 추가해줘야 함 //
         @RequestBody @Valid PatchValidationDto requestBody
     ) {
         return requestBody.getArg1();
@@ -126,8 +137,16 @@ public class MainController {
 
     @GetMapping("response-entity")
     public ResponseEntity<TmpResponseDto> getResponseEntity() {
-        TmpResponseDto responseBody = new TmpResponseDto("안녕하세요.", 10);
+        TmpResponseDto responseBody = new TmpResponseDto("안녕하세요", 10);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseBody);
     }
-    
+
+    @PostMapping("user")
+    public ResponseEntity<? super PostUserResponseDto> postUser(
+        @RequestBody @Valid PostUserRequestDto requestBody
+    ) {
+        ResponseEntity<? super PostUserResponseDto> response = mainService.postUser(requestBody);
+        return response;
+    }
+
 }
--- src/main/java/com/changseok/basic/service/MainService.java
+++ src/main/java/com/changseok/basic/service/MainService.java
@@ -1,7 +1,13 @@
 package com.changseok.basic.service;
 
+import org.springframework.http.ResponseEntity;
+
+import com.changseok.basic.dto.request.PostUserRequestDto;
+import com.changseok.basic.dto.response.PostUserResponseDto;
+
 public interface MainService {
     
     String getMethod();
+    ResponseEntity<? super PostUserResponseDto> postUser(PostUserRequestDto dto);
 
 }
